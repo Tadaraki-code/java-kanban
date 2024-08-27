45d146728ab5d73453427295b995bd9186f45d54
Sergey Andreev <siarhe1.andreeff@yandex.kz>
2024-08-27 15:19:22 +0300

    Добавить новый алгоритм хранения истории задач, основанный на двусвязном списке

--- src/test/ManagersTest.java
+++ src/test/ManagersTest.java
@@ -8,12 +8,12 @@ import static org.junit.jupiter.api.Assertions.*;
 class ManagersTest {
 
     @Test
-    public void ManagersShouldReturnReadyToWorkObjects () {
+    public void managersShouldReturnReadyToWorkObjects () {
         TaskManager manager = Managers.getDefault();
-        assertNotNull(manager.getTasksList());
+        assertNotNull (manager.getTasksList());
 
         HistoryManager historyManager = Managers.getDefaultHistory();
-        assertNotNull(historyManager.getHistory());
+        assertNotNull (historyManager.getHistory());
 
     }
 
