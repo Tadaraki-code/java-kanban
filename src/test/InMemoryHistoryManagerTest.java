3f76e71b9a6d60dd92fa6ac428ccfcefa928c33b
Sergey Andreev <siarhe1.andreeff@yandex.kz>
2024-08-27 15:27:08 +0300

    Добавить новый алгоритм хранения истории задач, основанный на двусвязном списке

--- src/test/InMemoryHistoryManagerTest.java
+++ src/test/InMemoryHistoryManagerTest.java
@@ -20,7 +20,7 @@ class InMemoryHistoryManagerTest {
         int taskOneId = manager.addNewTask(taskOne);
         manager.getTask(taskOneId);
 
-        taskOne = new Task("Task name changed", "Task description changed", taskOneId ,TaskStatus.IN_PROGRESS);
+        taskOne = new Task("Task name changed", "Task description changed", taskOneId, TaskStatus.IN_PROGRESS);
         manager.updateTask(taskOne);
 
         List<Task> historyList = manager.getHistory();
